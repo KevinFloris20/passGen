49572beba6314d28b6a50e289ccf830a73d0d82c
KevinFloris20 <75303163+KevinFloris20@users.noreply.github.com>
2021-01-22 01:47:52 -0500

    new pass gen updated

--- script.js
+++ script.js
@@ -1,165 +1,199 @@
+
+window.onload = function() { 
 //the vArs 
 var createbutton = document.querySelector("#Buttonfunction");
-var button1 = document.querySelector("#long");
-var button2 = document.querySelector("#medium");
-var button3 = document.querySelector("#short");
-var button4 = document.querySelector("#uppercase");
-var button5 = document.querySelector("#lowercase");
-var button6 = document.querySelector("#specialchar");
-var button7 = document.querySelector("#numbers");
+//-----------------------------------------------------------------------Old code-------------------------------
+// var button1 = document.querySelector("#long");
+// var button2 = document.querySelector("#medium");
+// var button3 = document.querySelector("#short");
+// var button4 = document.querySelector("#uppercase");
+// var button5 = document.querySelector("#lowercase");
+// var button6 = document.querySelector("#specialchar");
+// var button7 = document.querySelector("#numbers");
+// var length = document.getElementById('input').value;
+// var uppercase4 = 0;
+// var lowercase5 = 0;
+// var special6 = 0;
+// var numss7 = 0;
+//-----------------------------------------------------------------------Old code-------------------------------
+
+
+//generate the password
+function generatePassword() {
+//-----------------------------------------------------------------------Old code-------------------------------
+    // console.log(length);
+
+    // //length
+    // if(document.getElementById("long").disabled == true){
+    // var length = 24;
+    // }
+    // else if(document.getElementById("medium").disabled == true){
+    //     var length = 12;
+    //     }
+    //     else if(document.getElementById("short").disabled == true){
+    //         var length = 124;
+    //     }
+//-----------------------------------------------------------------------Old code-------------------------------
+
+
+//-----------------------------------------------------------------------New Code-------------------------------
+
+//Alerts
+var length = prompt("How long do you want your Password to be?", "8")
 var uppercase4 = 0;
 var lowercase5 = 0;
 var special6 = 0;
 var numss7 = 0;
 
+if(confirm("Do you want uppercase?")){
+    uppercase4++;
+}
+if(confirm("Do you want lowercase?")){
+    lowercase5++;
+}
+if(confirm("Do you want special characters?")){
+    special6++;
+}
+if(confirm("Do you want numbers?")){
+    numss7++;
+}
 
-//generate the password
-function generatePassword(){
-    console.log("click");
-
-    //length
-    if(document.getElementById("long").disabled == true){
-    var length = 24;
-    }
-    else if(document.getElementById("medium").disabled == true){
-        var length = 12;
-        }
-        else if(document.getElementById("short").disabled == true){
-            var length = 124;
-        }
-    
+//-----------------------------------------------------------------------New Code-------------------------------
     //char set
     var charset = "";
-    var cap =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var spchar = "@#!$&-~";
     var num = "0123456789";
     var lower = "abcdefghijklmnopqrstuvwxyz"
-    if(uppercase4 == 1){
+    if (uppercase4 == 1) {
         var charset = charset + cap;
     }
-    if(lowercase5 == 1){
+    if (lowercase5 == 1) {
         var charset = charset + lower;
     }
-    if(special6 == 1){
+    if (special6 == 1) {
         var charset = charset + spchar;
     }
-    if(numss7 == 1){
+    if (numss7 == 1) {
         var charset = charset + num;
     }
 
     //computation
-    var  retVal = "";
+    var retVal = "";
 
-    if(charset == "")
-    {
-        retVal ="ERROR"
+    if (charset === "") {
+        retVal = "ERROR"
     }
-    else
-    {
+    else {
         for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));}
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
     }
     console.log(retVal);
-    document.getElementById("textarea").innerHTML = retVal;
+    document.getElementById("textarea1").innerHTML = retVal;
     return retVal;
-    
-}
-createbutton.addEventListener("click",generatePassword);
 
-//make the long medium short buttons work together
-function buttonNature1(){
-      document.getElementById("long").disabled = true;
-      document.getElementById("short").disabled = false;
-      document.getElementById("medium").disabled = false;
-      if(document.getElementById("long").disabled == true){
-        document.getElementById("long").style.backgroundColor = 'red';
-        document.getElementById("short").style.backgroundColor = '#2cc732';
-        document.getElementById("medium").style.backgroundColor = '#2cc732';
-      }
 }
-function buttonNature2(){
-    document.getElementById("long").disabled = false;
-    document.getElementById("short").disabled = false;
-    document.getElementById("medium").disabled = true;
-    if(document.getElementById("medium").disabled == true){
-        document.getElementById("medium").style.backgroundColor = 'red';
-        document.getElementById("short").style.backgroundColor = '#2cc732';
-        document.getElementById("long").style.backgroundColor = '#2cc732';
-      }
-}
-function buttonNature3(){
-    document.getElementById("long").disabled = false;
-    document.getElementById("short").disabled = true;
-    document.getElementById("medium").disabled = false;
-    if(document.getElementById("short").disabled == true){
-        document.getElementById("short").style.backgroundColor = 'red';
-        document.getElementById("long").style.backgroundColor = '#2cc732';
-        document.getElementById("medium").style.backgroundColor = '#2cc732';
-      }
-}
-button1.addEventListener("click",buttonNature1);
-button2.addEventListener("click",buttonNature2);
-button3.addEventListener("click",buttonNature3);
+createbutton.addEventListener("click", generatePassword);
 
-//give the rest of the buttons some value
-function numbercasebutton(){
-    var butto1 = document.getElementById("numbers");//num
-    console.log("rawr");
-    if(numss7 == 0){
-        numss7++;
-        butto1.style.backgroundColor = 'red';
-    }
-    else{
-        numss7--;
-        butto1.style.backgroundColor = '#2cc732';
-    }
-    console.log(numss7);
-} 
 
-function uppercasebutton(){
-    var butto = document.getElementById("uppercase");//upercase
-    console.log("rawr");
-    if(uppercase4 == 0){
-        uppercase4++;
-        butto.style.backgroundColor = 'red';
-    }
-    else{
-        uppercase4--;
-        butto.style.backgroundColor = '#2cc732';
-    }
-    console.log(uppercase4);
-}
+//-----------------------------------------------------------------------Old code-------------------------------
+// //make the long medium short buttons work together
+// function buttonNature1() {
+//     document.getElementById("long").disabled = true;
+//     document.getElementById("short").disabled = false;
+//     document.getElementById("medium").disabled = false;
+//     if (document.getElementById("long").disabled == true) {
+//         document.getElementById("long").style.backgroundColor = 'red';
+//         document.getElementById("short").style.backgroundColor = '#2cc732';
+//         document.getElementById("medium").style.backgroundColor = '#2cc732';
+//     }
+// }
+// function buttonNature2() {
+//     document.getElementById("long").disabled = false;
+//     document.getElementById("short").disabled = false;
+//     document.getElementById("medium").disabled = true;
+//     if (document.getElementById("medium").disabled == true) {
+//         document.getElementById("medium").style.backgroundColor = 'red';
+//         document.getElementById("short").style.backgroundColor = '#2cc732';
+//         document.getElementById("long").style.backgroundColor = '#2cc732';
+//     }
+// }
+// function buttonNature3() {
+//     document.getElementById("long").disabled = false;
+//     document.getElementById("short").disabled = true;
+//     document.getElementById("medium").disabled = false;
+//     if (document.getElementById("short").disabled == true) {
+//         document.getElementById("short").style.backgroundColor = 'red';
+//         document.getElementById("long").style.backgroundColor = '#2cc732';
+//         document.getElementById("medium").style.backgroundColor = '#2cc732';
+//     }
+// }
+// button1.addEventListener("click", buttonNature1);
+// button2.addEventListener("click", buttonNature2);
+// button3.addEventListener("click", buttonNature3);
 
-function lowercasebutton(){
-    var butto2 = document.getElementById("lowercase");//lowercase
-    console.log("rawr");
-    if(lowercase5 == 0){
-        lowercase5++;
-        butto2.style.backgroundColor = 'red';
-    }
-    else{
-        lowercase5--;
-        butto2.style.backgroundColor = '#2cc732';
-    }
-    console.log(lowercase5);
-}
+// //give the rest of the buttons some value
+// function numbercasebutton() {
+//     var butto1 = document.getElementById("numbers");//num
+//     console.log("rawr");
+//     if (numss7 == 0) {
+//         numss7++;
+//         butto1.style.backgroundColor = 'red';
+//     }
+//     else {
+//         numss7--;
+//         butto1.style.backgroundColor = '#2cc732';
+//     }
+//     console.log(numss7);
+// }
 
-function specialcasebutton(){
-    var butto3 = document.getElementById("specialchar");//special
-    console.log("rawr");
-    if(special6 == 0){
-        special6++;
-        butto3.style.backgroundColor = 'red';
-    }
-    else{
-        special6--;
-        butto3.style.backgroundColor = '#2cc732';
-    }
-    console.log(special6);
-}
+// function uppercasebutton() {
+//     var butto = document.getElementById("uppercase");//upercase
+//     console.log("rawr");
+//     if (uppercase4 == 0) {
+//         uppercase4++;
+//         butto.style.backgroundColor = 'red';
+//     }
+//     else {
+//         uppercase4--;
+//         butto.style.backgroundColor = '#2cc732';
+//     }
+//     console.log(uppercase4);
+// }
 
-button4.addEventListener("click",uppercasebutton);  
-button5.addEventListener("click",lowercasebutton);  
-button6.addEventListener("click",specialcasebutton);  
-button7.addEventListener("click",numbercasebutton);  
- 
+// function lowercasebutton() {
+//     var butto2 = document.getElementById("lowercase");//lowercase
+//     console.log("rawr");
+//     if (lowercase5 == 0) {
+//         lowercase5++;
+//         butto2.style.backgroundColor = 'red';
+//     }
+//     else {
+//         lowercase5--;
+//         butto2.style.backgroundColor = '#2cc732';
+//     }
+//     console.log(lowercase5);
+// }
+
+// function specialcasebutton() {
+//     var butto3 = document.getElementById("specialchar");//special
+//     console.log("rawr");
+//     if (special6 == 0) {
+//         special6++;
+//         butto3.style.backgroundColor = 'red';
+//     }
+//     else {
+//         special6--;
+//         butto3.style.backgroundColor = '#2cc732';
+//     }
+//     console.log(special6);
+// }
+
+// button4.addEventListener("click", uppercasebutton);
+// button5.addEventListener("click", lowercasebutton);
+// button6.addEventListener("click", specialcasebutton);
+// button7.addEventListener("click", numbercasebutton);
+//-----------------------------------------------------------------------Old code-------------------------------
+
+};
